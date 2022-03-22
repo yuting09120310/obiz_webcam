a1f8440155a3df8b237dd8e61885d5784daadd56
yuting <yuting09120310@gmail.com>
2022-03-22 15:11:21 +0800

    完成

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,29 +28,23 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.Btn_Cam = new System.Windows.Forms.Button();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.Btn_save = new System.Windows.Forms.Button();
+            this.videoSourcePlayer1 = new AForge.Controls.VideoSourcePlayer();
+            this.pictureBox1 = new AForge.Controls.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
-            // pictureBox1
+            // Btn_Cam
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(12, 85);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(711, 479);
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(455, 32);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "Start";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.Btn_Cam.Location = new System.Drawing.Point(455, 32);
+            this.Btn_Cam.Name = "Btn_Cam";
+            this.Btn_Cam.Size = new System.Drawing.Size(75, 23);
+            this.Btn_Cam.TabIndex = 1;
+            this.Btn_Cam.Text = "Start";
+            this.Btn_Cam.UseVisualStyleBackColor = true;
+            this.Btn_Cam.Click += new System.EventHandler(this.Btn_Cam_Click);
             // 
             // comboBox1
             // 
@@ -60,14 +54,45 @@
             this.comboBox1.Size = new System.Drawing.Size(248, 20);
             this.comboBox1.TabIndex = 2;
             // 
+            // Btn_save
+            // 
+            this.Btn_save.Location = new System.Drawing.Point(647, 29);
+            this.Btn_save.Name = "Btn_save";
+            this.Btn_save.Size = new System.Drawing.Size(75, 23);
+            this.Btn_save.TabIndex = 3;
+            this.Btn_save.Text = "儲存";
+            this.Btn_save.UseVisualStyleBackColor = true;
+            this.Btn_save.Click += new System.EventHandler(this.Btn_save_Click);
+            // 
+            // videoSourcePlayer1
+            // 
+            this.videoSourcePlayer1.Location = new System.Drawing.Point(11, 61);
+            this.videoSourcePlayer1.Name = "videoSourcePlayer1";
+            this.videoSourcePlayer1.Size = new System.Drawing.Size(709, 365);
+            this.videoSourcePlayer1.TabIndex = 4;
+            this.videoSourcePlayer1.Text = "videoSourcePlayer1";
+            this.videoSourcePlayer1.VideoSource = null;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = null;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 376);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Visible = false;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(734, 576);
-            this.Controls.Add(this.comboBox1);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(734, 447);
             this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.videoSourcePlayer1);
+            this.Controls.Add(this.Btn_save);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.Btn_Cam);
             this.Name = "Form1";
             this.Text = "Form1";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
@@ -77,10 +102,11 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button Btn_Cam;
         private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button Btn_save;
+        private AForge.Controls.VideoSourcePlayer videoSourcePlayer1;
+        private AForge.Controls.PictureBox pictureBox1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using AForge.Video;
 using AForge.Video.DirectShow;
+using AForge.Controls;
 
 namespace obiz_webcam
 {
@@ -23,26 +24,29 @@ namespace obiz_webcam
 
         FilterInfoCollection USB_Webcams;
         VideoCaptureDevice Cam;
-
+        SaveFileDialog sfd = new SaveFileDialog();
 
         public void setup()
         {
             USB_Webcams = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            
             if (USB_Webcams.Count > 0) //如果有偵測到視訊鏡頭
             {
-                button1.Enabled = true;
+                Btn_Cam.Enabled = true;
             }
             else
             {
-                button1.Enabled = false;
+                Btn_Cam.Enabled = false;
                 MessageBox.Show("此機器沒有鏡頭");
             }
         }
 
-        //加入分辨率
+        //加入分辨率到comboxbox
         public void Add_Dpi()
         {
             Cam = new VideoCaptureDevice(USB_Webcams[0].MonikerString);
+
+            videoSourcePlayer1.VideoSource = Cam;
 
             VideoCapabilities[] videoCapabilities;
 
@@ -64,34 +68,27 @@ namespace obiz_webcam
         //設定dpi
         public void Set_Dpi()
         {
-            Cam = new VideoCaptureDevice(USB_Webcams[0].MonikerString);
-            Cam.VideoResolution = Cam.VideoCapabilities[comboBox1.SelectedIndex];
+            Cam = new VideoCaptureDevice(USB_Webcams[0].MonikerString);//選用第一個鏡頭
+            Cam.VideoResolution = Cam.VideoCapabilities[comboBox1.SelectedIndex]; //手動調整index
 
-            Cam.NewFrame += Cam_NewFrame;
+            videoSourcePlayer1.VideoSource = Cam;
         }
 
 
         //啟用或關閉
-        private void button1_Click(object sender, EventArgs e)
+        private void Btn_Cam_Click(object sender, EventArgs e)
         {
-            if (button1.Text == "Start")
+            if (Btn_Cam.Text == "Start")
             {
-                button1.Text = "Stop";
+                Btn_Cam.Text = "Stop";
                 Set_Dpi();
-                Cam.Start();
+                videoSourcePlayer1.Start();
             }
             else
             {
-                button1.Text = "Start";
-                Cam.Stop();
+                Btn_Cam.Text = "Start";
+                videoSourcePlayer1.Stop();
             }
-        }
-
-
-        //創建新畫面
-        private void Cam_NewFrame(object sender, NewFrameEventArgs eventArgs)
-        {
-            pictureBox1.Image = (Bitmap)eventArgs.Frame.Clone();
         }
 
 
@@ -101,6 +98,17 @@ namespace obiz_webcam
             Cam.Stop();
         }
 
-        
+
+        //儲存功能
+        private void Btn_save_Click(object sender, EventArgs e)
+        {
+            Bitmap img = videoSourcePlayer1.GetCurrentVideoFrame();
+            pictureBox1.Image = img;
+
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.Image.Save(sfd.FileName);
+            }
+        }
     }
 }
